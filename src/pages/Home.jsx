58a767748ff257c8b01a4bb76bf4cf13b5ce42df
Salem Shah <salemshahdev@gmail.com>
2024-06-11 07:24:45 +0200

    LP-6 the hero is finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Hero from "../components/Hero";
+import HomePagdData from '../data/HomePagdData.json';
+
 
 export default function Home() {
+    const {
+        hero, socialBtns, about, service, contact, approche
+    } = HomePagdData;
 
     return (<>
-
+        <Hero data={hero} socialData={socialBtns}/>
     </>);
 }
